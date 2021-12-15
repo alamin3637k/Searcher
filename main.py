e091a2b327ccfd31330793b5cb51c368574342b3
alamin3637k <89503433+alamin3637k@users.noreply.github.com>
2021-12-15 21:29:32 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,57 @@
 import webbrowser
-import pywhatkit
 import wikipedia
+import requests
 
-class sercher:
-    def __init__(self):
-        pass
+def yt_search(search: str):
+    webbrowser.open_new_tab(f"https://www.youtube.com/results?search_query={search}")
 
-    def yt_search(self, search):
-        webbrowser.open_new_tab(f"https://www.youtube.com/results?search_query={search}")
-    
-    def google_search(self, search):
-        webbrowser.open_new_tab(f"https://www.google.com/search?q={search}")
-    
-    def bing_search(self, search):
-        webbrowser.open_new_tab(f"https://www.bing.com/search?q={search}")
-    
-    def duck_search(self, search):
-        webbrowser.open_new_tab(f"https://duckduckgo.com/?q={search}")
-    
-    def yahoo_search(self, search):
-        webbrowser.open_new_tab(f"https://search.yahoo.com/search?p={search}")
-    
-    def ask_search(self, search):
-        webbrowser.open_new_tab(f"https://www.ask.com/web?q={search}")
-    
-    def yandex_search(self, search):
-        webbrowser.open_new_tab(f"https://yandex.com/search/?text={search}")
+def google_search(search: str):
+    webbrowser.open_new_tab(f"https://www.google.com/search?q={search}")
 
-    def ecosia_search(self, search):
-        webbrowser.open_new_tab(f"https://www.ecosia.org/search?q={search}")
-    
-    def facebook_search(self, search):
-        webbrowser.open_new_tab(f"https://www.facebook.com/search/top/?q={search}")
-    
-    def wiki_terminal_search(self, search, lang='en', sentence=1):
-        try:
-            wikipedia.set_lang(lang)
-            print(wikipedia.summary(search, sentences=sentence))
-        except Exception as error:
-            print(error)
-    
-    def mail_search(self, search):
-        webbrowser.open_new_tab(f"https://mail.google.com/mail/u/0/#search/{search}")
+def bing_search(search: str):
+    webbrowser.open_new_tab(f"https://www.bing.com/search?q={search}")
 
-    def palyonyt(self, search):
-        pywhatkit.playonyt(search)
+def duck_search(search: str):
+    webbrowser.open_new_tab(f"https://duckduckgo.com/?q={search}")
 
+def yahoo_search(search: str):
+    webbrowser.open_new_tab(f"https://search.yahoo.com/search?p={search}")
+
+def ask_search(search: str):
+    webbrowser.open_new_tab(f"https://www.ask.com/web?q={search}")
+
+def yandex_search(search: str):
+    webbrowser.open_new_tab(f"https://yandex.com/search/?text={search}")
+
+def ecosia_search(search: str):
+    webbrowser.open_new_tab(f"https://www.ecosia.org/search?q={search}")
+
+def fb_search(search: str):
+    webbrowser.open_new_tab(f"https://www.facebook.com/search/top/?q={search}")
+
+def wiki_terminal_search(search: str, lang='en', sentence=1):
+    try:
+        wikipedia.set_lang(lang)
+        print(wikipedia.summary(search, sentences=sentence))
+    except Exception as error:
+        print(error)
+        return error
+    
+def mail_search(search: str):
+    webbrowser.open_new_tab(f"https://mail.google.com/mail/u/0/#search/{search}")
+
+def wiki_web_search(search: str):
+    webbrowser.open_new_tab(f"https://en.wikipedia.org/wiki/{search}")
+
+def test_site(search: str):
+    """please enter site name with http information"""
+    try:
+        r = requests.get(search)
+
+    except Exception as error:
+        print(error)
+        return "site not working"
+    
+    if r.status_code == 200:
+        print("site working")
+        return "site working"
